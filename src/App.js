aeb9b3110f27791b1b18e158dbb7a57a599c8c40
Cem Tosun <134544650+Kilopi3142@users.noreply.github.com>
2023-10-02 11:29:47 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -11,17 +11,12 @@ function App() {
 
 	   <h2>Team</h2>
 	   <h3>Team member Cem Tosun : 0x386393C0A3A05343c4C9f76545f589b44038e22a</h3>
-	   <h3>Team member Eren Bahadır: 0x333Da10cACB66aB90CEc6D58ae660F4E127c1Beb</h3>
-	   <h3>Team member Sadık Emre Alkan: 0xA49E973712f1fC5BFaa61fcEC5327786E46CB42c</h3>
-	   <h3>Team member Han: 0x55Ea2eAA677a2acF4D838a758e11e5cBD408E6a1</h3>
 	   <h3>Team member Metin Avcı: 0xfa13502884839d557C8d3ff44311215461f1480c</h3>
 	   <h3>Team member Bilal Tekmil: 0x5E6c3cFfB89A71314CEc7dF98FDa92495E17B108</h3>
-	   <h3>Team member Bünyamin Doğan: 0xB1eA6308dAe50d138c27694279f587cA067611f2</h3>
-	   <h3>Team member Bahadırhan Güçnar: 0x0e4a6aF34CAF0E185953acFf9CEa01b54f2A5637</h3>
 	   <h3>Team member Yiğit Özyılmaz: 0xF73a35C1495abc2B006D4617F8ac1a64Affa2aF0</h3>
-	   <h3>Team member Emre Öztürk: 0xDC2b0832B37f8875d32Ff609da370A97cF3Ed6d3</h3>
-	   <h3>Team member Melek: 0x1D0f510c1cFee3b1e389539d3Ce52479EA54Db8D</h3>
 	   <h3>Team member Görkem Çetince: 0xF741fe3f06e1E0a4521f9347a4F3aaD2A75ec8Ee</h3>
+	   <h3>Team member Melek: 0x1D0f510c1cFee3b1e389539d3Ce52479EA54Db8D</h3>
+	   <h3>Team member Han: 0x55Ea2eAA677a2acF4D838a758e11e5cBD408E6a1</h3>
 	   
 	   
 	    <h2>Contracts</h2>
@@ -29,10 +24,6 @@ function App() {
 	   <h3>Deployer: 0xCF2346c4BFfB42DE8c28303f6ae7bFa59A30BA74</h3>
 	   <h3>Staking(NonLocked): 0xf1e683838aB750Afb0762cf28593378DC4C5997a</h3>
 	   <h3>Staking(Locked): 0x11652D7B587b7cffd5a3683A0d9991164c2DB0f3</h3>
-	   <h3>Migration: 0x4D729e447D2c775FF80f298CC974093ED90020d8</h3>
-	   <h3>Tranquil(Liquidity): 0x994A43610458005feC15280E39624d293B224590</h3>
-	   <h3>Masters of Management(Alexandra): 0x974Eed4Bdd519Ec31aFA4e9495024F76824a6E4a</h3>
-	   <h3>Masters of Management(Tristan): 0x050cf054426b30001a4B5662d7Ce175497EF7E60</h3>
 	   <h3>Pathfinder: 0x64491880c3f18276582e16D989943Aba6c141f04</h3>
 
 	
